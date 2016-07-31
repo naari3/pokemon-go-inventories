6a42c01c5cd404b49663f687c4de78d3b42d536c
naarisan <qazaqaza98@gmail.com>
2016-07-31 20:22:06 +0900

    delete not use code

--- FlaskWebProject/PgoInventories.py
+++ FlaskWebProject/PgoInventories.py
@@ -67,22 +67,6 @@ class PgoInventories(object):
 
         return (loc.latitude, loc.longitude, loc.altitude)
 
-    def get_cell_ids(self, lat, long, radius = 10):
-        origin = CellId.from_lat_lng(LatLng.from_degrees(lat, long)).parent(15)
-        walk = [origin.id()]
-        right = origin.next()
-        left = origin.prev()
-
-        # Search around provided radius
-        for i in range(radius):
-            walk.append(right.id())
-            walk.append(left.id())
-            right = right.next()
-            left = left.prev()
-
-        # Return everything
-        return sorted(walk)
-
     def encode(self, cellid):
         output = []
         encoder._VarintEncoder()(output.append, cellid)
